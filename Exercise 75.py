f4febb8e770c98dd32f81549c5e0aee13d547bd1
sajeeb-202 <sajeebkumar202@gmail.com>
2026-05-24 01:00:43 +0600

    Convert Two Lists into a Python Dictionary Efficiently

---
+++ Exercise 75.py
@@ -0,0 +1,15 @@
+def lists_to_dict(keys, values):
+    return dict(zip(keys, values))
+
+
+
+
+# def list2dict(keys, values):
+#     dict = {}
+#     for i in range(min(len(list1), len(list2))):
+#         dict[keys[i]] = values[i]
+#     return dict    
+
+# list1 = ['a', 'b', 'c', 'd']
+# list2 = [1,2,3,4]
+# print(list2dict(list1, list2))
