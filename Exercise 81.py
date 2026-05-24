79df641c2401a58d65c33ff2f25724b21cbbecfc
sajeeb-202 <sajeebkumar202@gmail.com>
2026-05-24 22:12:07 +0600

    Count Lines in a File Using Python

---
+++ Exercise 81.py
@@ -0,0 +1,7 @@
+def count_lines(file_path):
+    try:
+        with open(file_path, "r") as file:
+            return sum(1 for _ in file)
+        
+    except FileNotFoundError:
+        return _1    
